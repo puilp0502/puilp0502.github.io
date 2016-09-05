dabcbca62a0fee36608ac1bf4acc1cd3da1fb186
Frank Yang <puilp0502@gmail.com>
2016-09-05 21:11:34 +0900

    Create install-pip.sh

---
+++ install-pip.sh
@@ -0,0 +1,18 @@
+echo "#### Installing setuptools... ####"
+wget https://bootstrap.pypa.io/ez_setup.py -O - | sudo python > /dev/null
+if [[ ! $? ]]; then
+	echo "Failed!"
+	exit 1
+fi
+
+echo "#### Installing pip... ####"
+wget https://bootstrap.pypa.io/get-pip.py -O - | sudo python > /dev/null
+if [[ ! $? ]]; then
+	echo "Failed!"
+	exit 1
+fi
+
+if [[ $PATH != *"/usr/local/bin"* ]]; then
+	echo 'Adding /usr/local/bin to $PATH'
+	export PATH="/usr/local/bin:$PATH"
+fi
